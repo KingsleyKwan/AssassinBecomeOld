7137798619f9225ba75304c7f4a7997085b73581
Kingsley Kwan <a86248624a@hotmail.com>
2026-08-29 22:46:50 +0800

    Ch2 intro: org saw the alley; you were meant for this

--- chapters.js
+++ chapters.js
@@ -12,7 +12,7 @@ const CHAPTERS = [
     id: 2, numeral: "二", title: "入行", subtitle: "刀", weapon: "knife",
     enemyCount: 3, blocksToKill: 3, onlyPerfect: false, blockLabel: "擋",
     windupMs: 800, hitMs: 1500, perfectStartMs: 860, perfectEndMs: 1240, buttonPx: 104,
-    intro: ["殺手組織。佢哋話，你天生就係做呢行。", "第一晚出糧。倉庫入面三把刀。", "作出適當反應。失手一次，就冊第二次。"],
+    intro: ["上次件事，有個殺手組織嘅人見到。", "佢哋話：「你注定做呢行。」", "第一晚出糧。倉庫入面三把刀。"],
     resolve: "倉庫三把刀。你逐個放低三個人。組織收咗你。",
     resolveIf: "三個人倒下。你返到街上先想起後巷個 Yeesa。刀比拳頭快，人已經唔嗮度。",
   },
